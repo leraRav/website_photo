a790789fa8c7dd7784d1b8ca3be05f16562f5185
leraRav <lera.raspopova22@gmail.com>
2023-12-23 21:43:11 +0300

    scroll

--- script.js
+++ script.js
@@ -148,3 +148,16 @@ function init_slider(slider) {
 }
 
 
+$(function() {
+  $('a[href^="#"').on('click', function() {
+
+      let href = $(this).attr('href');
+  
+      $('html, body').animate({
+          scrollTop: $(href).offset().top
+      });
+      return false;
+  });
+
+
+});
